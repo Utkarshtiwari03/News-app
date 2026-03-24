eefc37ac4a903561e5be2087fda68970a15bda95
Utkarsh <utkarshtiwari20033@gmail.com>
2026-03-24 14:36:27 +0530

    Handling null result.

--- news/news_data/src/main/java/com/utkarsh/news_data/mapper/mappers.kt
+++ news/news_data/src/main/java/com/utkarsh/news_data/mapper/mappers.kt
@@ -5,10 +5,10 @@ import com.utkarsh.news_domain.model.Article
 
 fun ArticleDTO.toDomainArticle():Article{
     return Article(
-        author=this.author,
-        content=this.content,
-        description=this.description,
-        title=this.title,
-        urlToImage=this.urlToImage
+        author=this.author?:"author",
+        content=this.content?:"Missing content",
+        description=this.description?:"No description",
+        title=this.title?:"Title",
+        urlToImage=this.urlToImage?:"https://static.vecteezy.com/system/resources/thumbnails/057/068/323/small/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg"
     )
 }
--- news/news_data/src/main/java/com/utkarsh/news_data/repository/NewsRepositoryImpl.kt
+++ news/news_data/src/main/java/com/utkarsh/news_data/repository/NewsRepositoryImpl.kt
@@ -3,13 +3,20 @@ package com.utkarsh.news_data.repository
 import com.utkarsh.common_utils.Constants
 import com.utkarsh.news_data.mapper.toDomainArticle
 import com.utkarsh.news_data.network.NewsApiService
+import com.utkarsh.news_data.room.NewsDAO
 import com.utkarsh.news_domain.model.Article
 import com.utkarsh.news_domain.repository.NewsRepository
 
-class NewsRepositoryImpl(private val newsApiService: NewsApiService):NewsRepository {
+class NewsRepositoryImpl(private val newsApiService: NewsApiService,private val newsDAO: NewsDAO):NewsRepository {
     override suspend fun getNewsArticles(): List<Article> {
-        return newsApiService.getNewArticle().articles.map {
-            it.toDomainArticle()
+        try {
+            val temp= newsApiService.getNewArticle().articles.map {
+                it.toDomainArticle()
+            }
+            newsDAO.insertList(temp)
+            return newsDAO.getNewsArticle()
+        }catch (e:Exception){
+            return newsDAO.getNewsArticle()
         }
     }
 }
