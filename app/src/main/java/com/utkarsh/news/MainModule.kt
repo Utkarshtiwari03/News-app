f3a7219192b2f2c94ee2ef1ce0c2534039f4d4ed
Utkarsh <utkarshtiwari20033@gmail.com>
2026-03-24 14:37:01 +0530

    Implementing room DB.

--- app/src/main/java/com/utkarsh/news/MainModule.kt
+++ app/src/main/java/com/utkarsh/news/MainModule.kt
@@ -1,10 +1,14 @@
 package com.utkarsh.news
 
+import android.content.Context
 import com.utkarsh.common_utils.Navigator
 import com.utkarsh.news.navigation.DefaultNavigator
+import com.utkarsh.news.room.AppDatabase
+import com.utkarsh.news_data.room.NewsDAO
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -16,5 +20,16 @@ object MainModule {
     @Singleton
     fun provideProvider():Navigator.Provider{
         return DefaultNavigator()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsDatabase(@ApplicationContext context: Context):AppDatabase{
+        return AppDatabase.getInstance(context)
+    }
+
+    @Provides
+    fun providesNewsDAO(appDatabase: AppDatabase):NewsDAO{
+        return appDatabase.getNewsDao()
     }
 }
---
+++ news/news_data/src/main/java/com/utkarsh/news_data/room/NewsDAO.kt
@@ -0,0 +1,18 @@
+package com.utkarsh.news_data.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.utkarsh.news_domain.model.Article
+
+
+@Dao
+interface NewsDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertList(list:List<Article>)
+
+    @Query("SELECT * FROM ARTICLE")
+    suspend fun getNewsArticle():List<Article>
+}
